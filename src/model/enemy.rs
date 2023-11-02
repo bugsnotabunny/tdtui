776145eaf158409144af347a11ce0ae0bee54640
bugsnotabunny <malakhov.malakhit@gmail.com>
2023-11-02 13:17:41 +0300

    get rid of unnecessary macro

--- src/model/enemy.rs
+++ src/model/enemy.rs
@@ -15,41 +15,6 @@ pub trait Enemy: Drawable + UpdatableObject {
     fn reward(&self) -> u64;
 }
 
-macro_rules! decl_enemy_move_forward {
-    ($typename: ident) => {
-        impl UpdatableObject for $typename {
-            fn on_update(&mut self, game_model: &mut dyn GameModel, delta_time: Duration) {
-                self.move_forward(delta_time, game_model.trajectory());
-            }
-        }
-
-        impl $typename {
-            fn move_forward(&mut self, delta_time: Duration, trajectory: &dyn Trajectory) {
-                const INITIAL_STEP: f32 = 1e-3;
-                const EPSILON_MULTIPLYER: f32 = 1e2;
-                const EPSILON: f32 = f32::EPSILON * EPSILON_MULTIPLYER;
-
-                let mut move_points = self.speed * delta_time.as_secs_f32();
-                let mut step = INITIAL_STEP;
-                while move_points > EPSILON {
-                    let t_to_move_to = self.position + step;
-                    let self_pos = trajectory.get_point(self.position);
-                    let point_to_move_to = trajectory.get_point(t_to_move_to);
-                    let distance = self_pos.distance(&point_to_move_to);
-
-                    if distance > move_points {
-                        step /= 2.0;
-                        continue;
-                    }
-
-                    move_points -= step;
-                    self.position += step;
-                }
-            }
-        }
-    };
-}
-
 pub struct BasicEnemy {
     health: f32,
     speed: f32,
@@ -68,7 +33,45 @@ impl BasicEnemy {
     }
 }
 
-decl_enemy_move_forward!(BasicEnemy);
+impl UpdatableObject for BasicEnemy {
+    fn on_update(&mut self, game_model: &mut dyn GameModel, delta_time: Duration) {
+        self.position = move_forward_impl(
+            self.speed,
+            self.position,
+            delta_time,
+            game_model.trajectory(),
+        );
+    }
+}
+
+fn move_forward_impl(
+    speed: f32,
+    mut position: f32,
+    delta_time: Duration,
+    trajectory: &dyn Trajectory,
+) -> f32 {
+    const INITIAL_STEP: f32 = 1e-3;
+    const EPSILON_MULTIPLYER: f32 = 1e2;
+    const EPSILON: f32 = f32::EPSILON * EPSILON_MULTIPLYER;
+
+    let mut move_points = speed * delta_time.as_secs_f32();
+    let mut step = INITIAL_STEP;
+    while move_points > EPSILON {
+        let t_to_move_to = position + step;
+        let self_pos = trajectory.get_point(position);
+        let point_to_move_to = trajectory.get_point(t_to_move_to);
+        let distance = self_pos.distance(&point_to_move_to);
+
+        if distance > move_points {
+            step /= 2.0;
+            continue;
+        }
+
+        move_points -= step;
+        position += step;
+    }
+    position
+}
 
 impl Enemy for BasicEnemy {
     fn position(&self) -> f32 {
@@ -108,7 +111,16 @@ impl EnemyWithKinneticResist {
     }
 }
 
-decl_enemy_move_forward!(EnemyWithKinneticResist);
+impl UpdatableObject for EnemyWithKinneticResist {
+    fn on_update(&mut self, game_model: &mut dyn GameModel, delta_time: Duration) {
+        self.position = move_forward_impl(
+            self.speed,
+            self.position,
+            delta_time,
+            game_model.trajectory(),
+        );
+    }
+}
 
 impl Enemy for EnemyWithKinneticResist {
     fn position(&self) -> f32 {
@@ -152,7 +164,16 @@ impl EnemyWithMagicResist {
     }
 }
 
-decl_enemy_move_forward!(EnemyWithMagicResist);
+impl UpdatableObject for EnemyWithMagicResist {
+    fn on_update(&mut self, game_model: &mut dyn GameModel, delta_time: Duration) {
+        self.position = move_forward_impl(
+            self.speed,
+            self.position,
+            delta_time,
+            game_model.trajectory(),
+        );
+    }
+}
 
 impl Enemy for EnemyWithMagicResist {
     fn position(&self) -> f32 {
