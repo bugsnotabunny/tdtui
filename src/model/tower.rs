b355b96676b577bc40a9f753c760c513576ddc5a
bugsnotabunny <malakhov.malakhit@gmail.com>
2023-11-02 05:51:29 +0300

    generalize boilerplate code with macro

--- src/model/tower.rs
+++ src/model/tower.rs
@@ -51,19 +51,93 @@ impl Aim {
     }
 }
 
+macro_rules! create_tower_type {
+    ($a:ident ) => {
+        pub struct $a {
+            aim: Aim,
+            position: Point,
+            cooldown_clock: Clock,
+        }
+
+        impl $a {
+            pub fn new(position: Point) -> Self {
+                Self {
+                    aim: Aim::new(None),
+                    position: position,
+                    cooldown_clock: Clock::from_now(),
+                }
+            }
+
+            fn shoot(&mut self, game_model: &mut dyn GameModel) {
+                self.aim.try_shoot(Self::DAMAGE, |reward| {
+                    game_model.wallet_mut().add_money(reward);
+                });
+            }
+
+            fn update_aim(&mut self, game_model: &dyn GameModel) {
+                if !self.aim.is_in_shoot_range(self, game_model.trajectory()) {
+                    self.aim = Aim::new(None);
+                }
+
+                if self.aim.is_some() {
+                    return;
+                }
+
+                let random_chosen_enemy = game_model
+                    .enemies()
+                    .iter()
+                    .filter(|enemy| {
+                        let enemypos = game_model.trajectory().get_point(enemy.borrow().position());
+                        enemypos.distance(self.position()) < self.range()
+                    })
+                    .map(|rc| rc.clone())
+                    .choose(&mut rand::thread_rng());
+
+                self.aim = Aim::new(random_chosen_enemy);
+            }
+        }
+
+        impl Tower for $a {
+            fn position(&self) -> &Point {
+                &self.position
+            }
+
+            fn cost(&self) -> u64 {
+                Self::COST
+            }
+
+            fn range(&self) -> f32 {
+                Self::RANGE
+            }
+        }
+
+        impl UpdatableObject for $a {
+            fn on_update(&mut self, game_model: &mut dyn GameModel, _: Duration) {
+                self.update_aim(game_model);
+                if self.cooldown_clock.elapsed() > Self::COOLDOWN {
+                    self.shoot(game_model);
+                    self.cooldown_clock.tick();
+                }
+            }
+        }
+    };
+}
+
 pub trait Tower: UpdatableObject + Drawable {
     fn position(&self) -> &Point;
     fn range(&self) -> f32;
     fn cost(&self) -> u64;
 }
 
-pub struct ArcherTower {
-    aim: Aim,
-    position: Point,
-    cooldown_clock: Clock,
+pub trait TowerStats {
+    const COOLDOWN: Duration;
+    const COST: u64;
+    const RANGE: f32;
+    const DAMAGE: Damage;
 }
 
-impl ArcherTower {
+create_tower_type!(ArcherTower);
+impl TowerStats for ArcherTower {
     const COOLDOWN: Duration = Duration::from_millis(1500);
     const COST: u64 = 10;
     const RANGE: f32 = 50.0;
@@ -71,57 +145,10 @@ impl ArcherTower {
         value: 10,
         kind: DamageType::Kinnetic,
     };
-
-    pub fn new(position: Point) -> Self {
-        Self {
-            aim: Aim::new(None),
-            position: position,
-            cooldown_clock: Clock::from_now(),
-        }
-    }
-
-    fn shoot(&mut self, game_model: &mut dyn GameModel) {
-        shoot_impl(&mut self.aim, Self::DAMAGE, game_model);
-    }
-
-    fn update_aim(&mut self, game_model: &dyn GameModel) {
-        let mut aim = std::mem::take(&mut self.aim);
-        update_aim_impl(&mut aim, self, game_model);
-        self.aim = aim;
-    }
 }
 
-impl Tower for ArcherTower {
-    fn position(&self) -> &Point {
-        &self.position
-    }
-
-    fn cost(&self) -> u64 {
-        Self::COST
-    }
-
-    fn range(&self) -> f32 {
-        Self::RANGE
-    }
-}
-
-impl UpdatableObject for ArcherTower {
-    fn on_update(&mut self, game_model: &mut dyn GameModel, _: Duration) {
-        self.update_aim(game_model);
-        if self.cooldown_clock.elapsed() > Self::COOLDOWN {
-            self.shoot(game_model);
-            self.cooldown_clock.tick();
-        }
-    }
-}
-
-pub struct MageTower {
-    aim: Aim,
-    position: Point,
-    cooldown_clock: Clock,
-}
-
-impl MageTower {
+create_tower_type!(MageTower);
+impl TowerStats for MageTower {
     const COOLDOWN: Duration = Duration::from_millis(2000);
     const COST: u64 = 20;
     const RANGE: f32 = 100.0;
@@ -129,74 +156,4 @@ impl MageTower {
         value: 5,
         kind: DamageType::Magic,
     };
-
-    pub fn new(position: Point) -> Self {
-        Self {
-            aim: Aim::new(None),
-            position: position,
-            cooldown_clock: Clock::from_now(),
-        }
-    }
-
-    fn shoot(&mut self, game_model: &mut dyn GameModel) {
-        shoot_impl(&mut self.aim, Self::DAMAGE, game_model);
-    }
-
-    fn update_aim(&mut self, game_model: &dyn GameModel) {
-        let mut aim = std::mem::take(&mut self.aim);
-        update_aim_impl(&mut aim, self, game_model);
-        self.aim = aim;
-    }
-}
-
-impl Tower for MageTower {
-    fn position(&self) -> &Point {
-        &self.position
-    }
-
-    fn cost(&self) -> u64 {
-        Self::COST
-    }
-
-    fn range(&self) -> f32 {
-        Self::RANGE
-    }
-}
-
-impl UpdatableObject for MageTower {
-    fn on_update(&mut self, game_model: &mut dyn GameModel, _: Duration) {
-        self.update_aim(game_model);
-        if self.cooldown_clock.elapsed() > Self::COOLDOWN {
-            self.shoot(game_model);
-            self.cooldown_clock.tick();
-        }
-    }
-}
-
-fn shoot_impl(aim: &mut Aim, damage: Damage, game_model: &mut dyn GameModel) {
-    aim.try_shoot(damage, |reward| {
-        game_model.wallet_mut().add_money(reward);
-    });
-}
-
-fn update_aim_impl(aim: &mut Aim, tower: &impl Tower, game_model: &dyn GameModel) {
-    if !aim.is_in_shoot_range(tower, game_model.trajectory()) {
-        *aim = Aim::new(None);
-    }
-
-    if aim.is_some() {
-        return;
-    }
-
-    let random_chosen_enemy = game_model
-        .enemies()
-        .iter()
-        .filter(|enemy| {
-            let enemypos = game_model.trajectory().get_point(enemy.borrow().position());
-            enemypos.distance(&tower.position()) < tower.range()
-        })
-        .map(|rc| rc.clone())
-        .choose(&mut rand::thread_rng());
-
-    *aim = Aim::new(random_chosen_enemy);
 }
